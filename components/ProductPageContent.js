5822cca58c282a6fcad0990af0426702a4d4577d
onlyjayk08 <86417009+onlyjayk08@users.noreply.github.com>
2022-03-02 09:50:43 -0500

    product update

--- components/ProductPageContent.js
+++ components/ProductPageContent.js
@@ -1,36 +1,36 @@
 import Image from 'next/image'
 import ProductForm from './ProductForm'
 import RecommendedList from './RecommendedList'
-import Slider from 'react-slick';
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+// import Slider from 'react-slick';
+// import "slick-carousel/slick/slick.css";
+// import "slick-carousel/slick/slick-theme.css";
 
 export default function ProductPageContent({ product }) {
 
-  const images = []
+  // const images = []
 
-  product.images.edges.map((image, i) => {
-    images.push(
-      {
-        src: image.node.originalSrc,
-        key: `image-${image.node.originalSrc}`
-      }
-    )
-  })
+  // product.images.edges.map((image, i) => {
+  //   images.push(
+  //     {
+  //       src: image.node.originalSrc,
+  //       key: `image-${image.node.originalSrc}`
+  //     }
+  //   )
+  // })
 
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1
-  };
+  // const settings = {
+  //   dots: true,
+  //   infinite: true,
+  //   speed: 500,
+  //   slidesToShow: 1,
+  //   slidesToScroll: 1
+  // };
 
   return (
     <div>
       <div>
         <div>
-          <div className='pb-10'>
+          {/* <div className='pb-10'>
             <Slider {...settings}>
               {images.map((slide) => (
                 <div key={slide.key}>
@@ -38,7 +38,7 @@ export default function ProductPageContent({ product }) {
                 </div>
               ))}
             </Slider>
-          </div>
+          </div> */}
         </div>
         <ProductForm product={product} />
       </div>
