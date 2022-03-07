4096421c4e6313f53a0a11d6624f7cdcdfe23e27
onlyjayk08 <86417009+onlyjayk08@users.noreply.github.com>
2022-03-07 15:16:37 -0500

    customer info and logout

--- pages/account/login.js
+++ pages/account/login.js
@@ -1,12 +1,22 @@
 import { useState } from "react";
 import { customerAccessTokenCreate } from "../../lib/shopify";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function Login(){
+    const router = useRouter();
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const [accessToken, setAccessToken] = useState("");
+    const [loginError, setLoginError] = useState("");
+
+    useEffect(()=>{
+        if(localStorage.accessToken){
+            const accesstoken = localStorage.accessToken
+            console.log(accesstoken)
+            router.push('/customer')
+        }
+    },[])
 
     const handleLogin = async (e) => {
         e.preventDefault();
@@ -17,12 +27,17 @@ export default function Login(){
         }
         const data = await customerAccessTokenCreate(input);
         console.log(data)
-        {data.customerAccessToken ? (setAccessToken(data.customerAccessToken.accessToken)) : null}
+        if (data.customerAccessToken) {
+            localStorage.setItem("accessToken", data.customerAccessToken.accessToken)
+            router.push('/customer')
+        }
+        {data.customerUserErrors ? (setLoginError(data.customerUserErrors.message)) : null}
     }
 
     return(
         <div>
             <form onSubmit={handleLogin}>
+                {loginError ? {loginError} : null}
                 <label className="px-4">Enter your Email</label>
                 <input type="email" id="email" placeholder="Email" name={"email"} value={email} onChange={(event) => { setEmail(event.target.value) }} required />
                 <br />
@@ -36,7 +51,6 @@ export default function Login(){
                     or Create an Account
                 </a>
             </Link>
-            {accessToken ? <p>{accessToken}</p>: <p>error</p>}
         </div>
     )
 }
---
+++ pages/customer.js
@@ -0,0 +1,47 @@
+import { useState, useEffect } from "react"
+import { useRouter } from "next/router";
+import { getCustomer } from "../lib/shopify";
+
+export default function Customer(){
+    const router = useRouter();
+
+    const [accessToken, setAccessToken] = useState("");
+    const [customer, setCustomer] = useState("");
+
+    const handleLogout = async () =>{
+
+        localStorage.removeItem("accessToken")
+        router.push('account/login')
+    }
+
+    useEffect(async ()=>{
+        if(localStorage.accessToken){
+            setAccessToken(localStorage.accessToken)
+            const customer = await getCustomer(localStorage.accessToken)
+            setCustomer(customer)
+            console.log(customer)
+        }
+        else{
+            router.push('/account/login')
+        }
+        
+    },[])
+    
+
+    return(
+        <div>
+            <p>Customer page</p>
+            {customer? (<div>
+                {accessToken}
+                <p>Display Name: {customer.displayName}</p>
+                <p>First Name: {customer.firstName}</p>
+                <p>Last Name: {customer.lastName}</p>
+                <p>Email: {customer.email}</p>
+
+                <button onClick={handleLogout}>    
+                    Logout
+                </button>
+            </div>): null}
+        </div>
+    )
+}
