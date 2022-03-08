312ece1bcc065656a38a89c7e9e7504d1c7185b0
onlyjayk08 <86417009+onlyjayk08@users.noreply.github.com>
2022-03-08 16:24:00 -0500

    minor issue

--- components/CustomerOrder.js
+++ components/CustomerOrder.js
@@ -7,7 +7,7 @@ export default function CustomerOrder({ orders }){
             <h2 className="text-2xl py-3">Order List</h2>
             {orders.map((order, i) => (
                 <div key={`order-${i}`}>
-                    <h3 class="text-xl bold p-2">{order.node.name}</h3>
+                    <h3 className="text-xl bold p-2">{order.node.name}</h3>
                     <OrderDetail order={order}/>
                 </div>
             ))}
